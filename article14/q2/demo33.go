9522d05e6802f3ff25b61e52ad1574b299c5e46e
ShareLinux <sharelinuxs@gmail.com>
2019-02-20 12:14:22 +0800

    第十四讲 接口类型的合理运用

---
+++ article14/q2/demo33.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Pet interface {
+	Name() string
+	Category() string
+}
+type Dog struct {
+	name string // 名字
+}
+
+func (dog *Dog) SetName(name string) {
+	dog.name = name
+}
+
+func (dog Dog) Name() string {
+	return dog.name
+}
+
+func (dog Dog) Category() string {
+	return "dog"
+}
+
+func main() {
+	// 示例1
+	var dog1 *Dog
+	fmt.Printf("The first dog is nil.\n")
+	dog2 := dog1
+	fmt.Printf("The second dog is nil.\n")
+	var pet Pet = dog2
+	if pet == nil {
+		fmt.Println("The pet is nil.")
+	} else {
+		fmt.Println("The pet is not nil.")
+	}
+	fmt.Printf("The type of pet is %T.\n", pet)
+	fmt.Printf("The type of pet is %s.\n", reflect.TypeOf(pet).String())
+	fmt.Printf("The type if second dog is %T.\n", dog2)
+	fmt.Println()
+
+	// 示例2
+	wrap := func(dog *Dog) Pet {
+		if dog == nil {
+			return nil
+		}
+		return dog
+	}
+	pet = wrap(dog2)
+	if pet == nil {
+		fmt.Println("The pet is nil.")
+	} else {
+		fmt.Println("The pet is not nil.")
+	}
+}
---
+++ article14/q3/demo34.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	ScientificName() string // 动物的学名
+	Category() string       // 动物的分类
+}
+
+type Named interface {
+	// 用户获取名字
+	Name() string
+}
+
+type Pet interface {
+	Animal
+	Named
+}
+
+type PetTag struct {
+	name  string
+	owner string
+}
+
+func (pt PetTag) Name() string {
+	return pt.name
+}
+
+func (pt PetTag) Owner() string {
+	return pt.owner
+}
+
+type Dog struct {
+	PetTag
+	scientificName string
+}
+
+func (dog Dog) ScientificName() string {
+	return dog.scientificName
+}
+
+func (dog Dog) Category() string {
+	return "dog"
+}
+
+func main() {
+	petTag := PetTag{name: "little pig"}
+	_, ok := interface{}(petTag).(Named)
+	fmt.Printf("PegTag implements interface Named: %v\n", ok)
+
+	dog := Dog{
+		PetTag:         petTag,
+		scientificName: "Labrador Retriever",
+	}
+	_, ok = interface{}(dog).(Animal)
+	fmt.Printf("Dog implements interface Animal: %v\n", ok)
+	_, ok = interface{}(dog).(Named)
+	fmt.Printf("Dog implements interface Named: %v\n", ok)
+	_, ok = interface{}(dog).(Pet)
+	fmt.Printf("Dog implements interface Pet: %v\n", ok)
+
+}
